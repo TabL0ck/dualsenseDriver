009799c851519792be8d18bc679ff501c23ca366
TabL0ck <miscritscrits@gmail.com>
2023-10-09 07:18:42 +0300

    remove cache folder

---
+++ Build/dualsense/dualsense.d
@@ -0,0 +1,3 @@
+/home/lena/Documents/dualSenseDriver/Build/dualsense/dualsense.o: \
+ /home/lena/Documents/dualSenseDriver/Source/dualsense/dualsense.cpp \
+ /home/lena/Documents/dualSenseDriver/Source/dualsense/dualsense.hpp
---
+++ Build/main/dualsenseDriver.d
@@ -0,0 +1,3 @@
+/home/lena/Documents/dualSenseDriver/Build/main/dualsenseDriver.o: \
+ /home/lena/Documents/dualSenseDriver/Source/main/dualsenseDriver.cpp \
+ /home/lena/Documents/dualSenseDriver/Source/dualsense/dualsense.hpp
